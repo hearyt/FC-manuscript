91b69f1e8a355da82a170eb728442817073f6bda
Ting Yang <tyang6@jh.edu>
2017-10-25 11:51:27 -0400

    restructured data processing file.

--- rcode_0b.R
+++ rcode_0b.R
@@ -1,13 +1,14 @@
 
-###############
-## load data ##
-###############
+#######################
+## 1. load flow data ##
+#######################
 
 folder = "~/Box Sync/Ting's Lyme Research Folder 2016/Projects/Flow Data/Datasets/"
 
 ## load codebook
 
-code = read.xlsx(paste0(folder, "Other Data/codebook.xlsx"), sheetIndex = 1, stringsAsFactors = FALSE)
+code = read.xlsx(paste0(folder, "Other Data/codebook.xlsx"), 
+                 sheetIndex = 1, stringsAsFactors = FALSE)
 
 ## load flow data
 
@@ -28,27 +29,32 @@ df.list = lapply(df.list, function(x) { x["bleed.date"] <- NULL; x })
 d = bind_rows(df.list)
 dim(d)
 
-## remove trailing space or leading space from pid
+## remove trailing space or leading space from pid and visit
 
 d$pid = trimws(d$pid, which = "both")
+d$visit = trimws(d$visit, which = "both")
 
 ## load tracking sheet PIDs
 
 tracking = read.xlsx(paste0(folder, "Other Data/tracking.xlsx"), sheetIndex = 1)
 
-## load SLICE III clinical data
-
-d.c3 = read.csv(paste0(folder, "Other Data/alldata3.2017.07.07.csv")) # clinical data
-names(d.c3) = tolower(names(d.c3))
+#################################################
+## 2. load clinical data: SLICE II & SLICE III ##
+#################################################
 
 ## load SLICE II clinical data
 
 d.c2 = read.csv(paste0(folder, "Other Data/alldata2.2017.06.01.csv")) # clinical data
 names(d.c2) = tolower(names(d.c2))
 
-##################################################################
-## remove values based on Aarti's flow cytometry tracking sheet ##
-##################################################################
+## load SLICE III clinical data
+
+d.c3 = read.csv(paste0(folder, "Other Data/alldata3.2017.07.07.csv")) # clinical data
+names(d.c3) = tolower(names(d.c3))
+
+#####################################################################
+## 3. remove values based on Aarti's flow cytometry tracking sheet ##
+#####################################################################
 
 ## remove 07-196, 07-258 
 
@@ -121,13 +127,37 @@ ind = which(d$pid %in% c("07-208",
 
 d[ind ,c(names(d)[grep("custom.token", names(d))])] = NA
 
-#####################
-## data processing ##
-#####################
+##############################
+## 4. data processing: flow ##
+##############################
 
 ## flow data: extract group variable
 
 d$group = substring(d[,1], 1, 2)
+
+## remove these pids: '07-195', '07-204', '07-283', '07-297', '07-196'
+
+ind = which(d$pid %in% c("07-195", "07-204", "07-283", "07-297", "07-196"))
+d = d[-ind, ]
+
+## only keep visit 1 data
+
+d = d[which(d$visit=="V1"), ]
+dim(d)
+length(unique(d$pid))
+
+## calculate total memory
+
+d$cd4.pos.tm = d$cd4.pos.cm + d$cd4.pos.em + d$cd4.pos.emra
+d$cd8.pos.tm = d$cd8.pos.cm + d$cd8.pos.em + d$cd8.pos.emra
+
+## layer info
+
+code$layer = as.numeric(code$layer)
+
+##################################
+## 5. data processing: clinical ##
+##################################
 
 ## SLICE II: extract control data
 
@@ -140,38 +170,22 @@ d.c3$group = substring(d.c3[,1], 1, 2)
 
 ## remove these pids: '07-195', '07-204', '07-283', '07-297', '07-196'
 
-ind = which(d$pid %in% c("07-195", "07-204", "07-283", "07-297", "07-196"))
-d = d[-ind, ]
-
 ind = which(d.c3$pid %in% c("07-195", "07-204", "07-283", "07-297", "07-196"))
 d.c3 = d.c3[-ind, ]
 
-## calculate total memory
-
-d$cd4.pos.tm = d$cd4.pos.cm + d$cd4.pos.em + d$cd4.pos.emra
-d$cd8.pos.tm = d$cd8.pos.cm + d$cd8.pos.em + d$cd8.pos.emra
-
-## layer info
-
-code$layer = as.numeric(code$layer)
-
-########################
-## data quality check ##
-########################
+#################################
+## 6. data quality check: flow ##
+#################################
 
 ## check against tracking sheet
 
 length(unique(tracking$pid))
-length(unique(d$pid))
+length(unique(d$pid)) # controls and ptlds
 sum( unique(d$pid) %in% tracking$pid )
 
-## add a column indicating the person that gated the sample
-
-d$person.gate = "aarti"
-
-########################################################
-## combine PTLDS and controls clinical data: a subset ##
-########################################################
+################################################
+## 7. combine clinical data: PTLDS & controls ##
+################################################
 
 ## variables needed 
 
@@ -182,7 +196,7 @@ vars.c = c("pid",
            "rash_mr", "carditis_mr", "neuro_mr", "arthritis_mr", 
            "time_tx", "pretx_inax", 
            "educa_dg", "hispa_dg", "race1_dg", "race2_dg",
-           "age", "gender", "group", 
+           "age", "gender",
            "lys2e_mr", "lys2m_mr", "lys2g_mr", "lys2c_mr",
            "fzip3_dg", 
            "tickb_lh", 
@@ -198,7 +212,21 @@ vars.c2 = vars.c[which(vars.c %in% names(d.c2))]
 temp1 = d.c3[,vars.c]
 temp2 = d.c2[,vars.c2]
 d.c = merge(temp1, temp2, all=TRUE)
+# d.c$group = substring(d.c$pid, 1, 2)
 
-## save dataset for later use
+#######################################
+## 8. combine flow and clinical data ##
+#######################################
 
-d.o = d.c
+## overlapping pids only
+
+pid = intersect(unique(d$pid), unique(d.c$pid))
+
+## merge flow and clinical data
+
+d.all = merge(d, d.c, by="pid", all=FALSE)
+
+dim(d.all)
+length(unique(d.all$pid))
+table(d.all$group)
+
